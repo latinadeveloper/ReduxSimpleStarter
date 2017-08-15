362a2ef84bab33452e79ff8f808200e7bd8bc562
Isis Tejeda <isis@isistejeda.com>
2017-08-15 11:02:31 -0500

    you tube search api and bar setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyCgRXCoCKqfVqXJjDWCUi4gfZB77msDiew"
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+    return (
+        <div>
+          <SearchBar/>
+        </div>
+    );
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+ReactDOM.render(<App />, document.querySelector('.container'))
